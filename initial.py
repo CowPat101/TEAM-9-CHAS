d306cc74ba27ae0b58c5338561c909a1f9d2809c
francescotamburi <79522605+francescotamburi@users.noreply.github.com>
2023-10-07 02:27:36 +0100

    Initial.py code cleanup
    - Commented and cleaned up code
    - Added code to potentially fix sound lagging when pressing buttons

--- initial.py
+++ initial.py
@@ -1,55 +1,84 @@
+#import necessary modules
 import pygame
 import time
 
-clock = pygame.time.Clock()
-
-pygame.init()
-
-display_width = 800
-display_height = 600
-
-s1 = pygame.mixer.Sound("placeholder sounds/beep1.ogg")
-s2 = pygame.mixer.Sound("placeholder sounds/beep2.ogg")
-s3 = pygame.mixer.Sound("placeholder sounds/beep3.ogg")
-s4 = pygame.mixer.Sound("placeholder sounds/beep4.ogg")
-s5 = pygame.mixer.Sound("placeholder sounds/beep5.ogg")
-
-pause = False
-
 def quitgame():
-	pygame.quit()
-	quit()
+    pygame.quit()
+    quit()
 
-def loop():
-	paused = False
-	
-	pygame.mixer.music.load("placeholder sounds/simple-loop.ogg")
-	pygame.mixer.music.play(-1)
-	
-	while not paused:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				quitgame()
-			
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_a:
-					pygame.mixer.Sound.play(s1)
-				if event.key == pygame.K_s:
-					pygame.mixer.Sound.play(s2)
-				if event.key == pygame.K_d:
-					pygame.mixer.Sound.play(s3)
-				if event.key == pygame.K_f:
-					pygame.mixer.Sound.play(s4)
-				if event.key == pygame.K_g:
-					pygame.mixer.Sound.play(s5)
-				if event.key == pygame.K_p:
-					if paused:
-						pygame.mixer.music.unpause()
-					if not paused:
-						pygame.mixer.music.pause()
-		
-		pygame.display.update()
-		clock.tick(240)
+#gets sounds from source files and loads them into mixer so they can be recalled
+def load_sounds(source_list):
+    sounds = []
+    
+    for s in source_list:
+        sounds.append(pygame.mixer.Sound(s))
+    
+    return sounds
 
-pygame.display.set_mode((display_width,display_height))
-loop()
+#contains code that only needs to be executed when running this .py file as standalone program
+def main():
+    pygame.mixer.pre_init() #dark magic to ward away input lag, must be cast before pygame.init()
+    pygame.init()
+    
+    #display settings
+    display_width = 800
+    display_height = 600
+    pygame.display.set_mode((display_width,display_height))	
+
+#main game function
+def game():
+    
+    clock = pygame.time.Clock() #will be used eventually to set framerate
+    
+    paused = False
+    
+    #actually linking sounds from source files (ideally this would be done through some sort of settings file)
+    sound_sources = ["placeholder_sounds/beep1.ogg",
+                     "placeholder_sounds/beep2.ogg",
+                     "placeholder_sounds/beep3.ogg",
+                     "placeholder_sounds/beep4.ogg",
+                     "placeholder_sounds/beep5.ogg"]
+    sound = load_sounds(sound_sources)
+    
+    pygame.mixer.music.load("placeholder_sounds/simple-loop.ogg") #sets music
+    pygame.mixer.music.play(-1) #"-1" plays music indefinitely
+    
+    #sets keybindings (again, ideally settings file)
+    keybinds = {"s0": pygame.K_a,
+                "s1": pygame.K_s,
+                "s2": pygame.K_d,
+                "s3": pygame.K_f,
+                "s4": pygame.K_g,
+                "pause": pygame.K_p,
+                }
+    
+    #main game loop
+    while not paused:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+            
+            #handles button presses
+            if event.type == pygame.KEYDOWN:
+                if event.key == keybinds["s0"]:
+                    pygame.mixer.Sound.play(sound[0])
+                if event.key == keybinds["s1"]:
+                    pygame.mixer.Sound.play(sound[1])
+                if event.key == keybinds["s2"]:
+                    pygame.mixer.Sound.play(sound[2])
+                if event.key == keybinds["s3"]:
+                    pygame.mixer.Sound.play(sound[3])
+                if event.key == keybinds["s4"]:
+                    pygame.mixer.Sound.play(sound[4])
+                if event.key == keybinds["pause"]:
+                    if paused:
+                        pygame.mixer.music.unpause()
+                    if not paused:
+                        pygame.mixer.music.pause()
+        
+        pygame.display.update()
+        clock.tick(240) #sets framerate
+
+if __name__=="__main__":
+    main()
+    game()
