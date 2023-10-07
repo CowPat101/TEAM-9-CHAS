bcb429a97b69e93e44752fcbedd877f04b634292
CowPat101 <61464936+CowPat101@users.noreply.github.com>
2023-10-07 17:38:49 +0100

    Update Main.py
    Removed game_loop

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import time
 import pygame_menu as pm
 import colours as cs
 
+
 pygame.mixer.pre_init()
 pygame.init()
 window_width = 800  # Set your window dimensions
@@ -42,26 +43,29 @@ def display_game_menu():
                         action=tutorial_mode, align=pm.locals.ALIGN_CENTER)
     # Create a back button to return to the main menu
     game_menu.add.button(title="Return To Main Menu", 
-                        action=game_loop, align=pm.locals.ALIGN_CENTER) 
+                        action=display_main_menu, align=pm.locals.ALIGN_CENTER) 
     # Start the menu
     game_menu.mainloop(screen)
 
 def display_game_screen():
     # load background image to the screen
-    background = pygame.image.load("placeholder_sprites\IMG_5565.jpeg")
+    background = pygame.image.load("IMG_5565.jpeg")
     screen.blit(background, (0, 0))
+    pygame.display.flip()  # update the screen
+
+    # game loop to keep the window open
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
 def campaign_mode():
     display_game_screen()
-    
     pass
-
-
 def freestyle_mode():
     display_game_screen()
     pass
-
-
 def tutorial_mode():
     display_game_screen()
     pass
@@ -75,7 +79,7 @@ def display_settings_menu():
 
     # Create a back button to return to the main menu
     settings_menu.add.button(title="Return To Main Menu", 
-                        action=game_loop, align=pm.locals.ALIGN_CENTER) 
+                        action=display_main_menu, align=pm.locals.ALIGN_CENTER) 
 
     # Start the settings menu
     settings_menu.mainloop(screen)
@@ -99,105 +103,6 @@ def draw_back_button():
             go_back = True
             return go_back   
 
-#Main Logic function of the program
-def game_loop():
-
-    text_to_display = ""
-    display_text = False
-    text_start_time = 0
-
-    #Function to display a subtitle to the screen while allowing complete interaction with the game
-    def display():
-        if time.time() - text_start_time < 3:  # Display text for 3 seconds
-            #Choose a font and size
-            my_font = pygame.font.SysFont('Comic Sans MS', 30) 
-
-            # Make the text and text box surfaces
-            text_surface = my_font.render('Some Text', False, cs.BLACK)  #Change font colour at the end
-            text_rect = text_surface.get_rect() 
-
-            # Create a new surface for the text box (Background colour)
-            text_box = pygame.Surface((text_rect.width, text_rect.height))
-            text_box.set_alpha(128)  # Set the alpha value of the color to make it transparent
-            text_box.fill(cs.GREY)  # Fill with light gray color
-
-            #Modify the screen alignment of the text box
-
-            # Get the rectangle of the text surface and center it in the text box surface
-            text_rect.center = text_box.get_rect().center
-
-            # Blit the text surface onto the text box surface
-            text_box.blit(text_surface, text_rect)
-
-            # Get the new rectangle of the text surface and center it in the screen surface
-            text_rect = text_box.get_rect()
-            text_rect.midbottom = screen.get_rect().midbottom
-
-            # Blit the text box surface onto the main screen surface
-            screen.blit(text_box, text_rect)
-        else:
-            display_text = False   
-
-    #Main game loop for interaction with the game - be it display subtitles, access a menu, hit a beat etc. 
-     
-    #Set up identifiers for the loop for exiting the main page
-    running = True
-    go_back = False
-    exit_loop = False
-    go_settings = False
-    go_main = False
-    game = False
-
-    while running:
-        #These for loops look for keyboard inputs for their functions to run
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:  
-                    display_text = True
-                    text_start_time = time.time()
-            elif event.type == pygame.MOUSEBUTTONDOWN: # Go to settings from mouseclick on game screen
-                if event.button == 3:
-                    running = False
-                    exit_loop = True
-                    go_settings = True
-            else:
-                if game:
-                    game.eventhandler(event)
-
-        #This loop runs the main game screen - WRITE YOUR GAME CODE IN THIS IF STATEMENT - FOR THE GAME SCREEN            
-        if exit_loop == False:
-
-            screen.fill((0, 0, 0))  
-
-            #Get the action from the back button on the game screen
-            go_back = draw_back_button()
-
-            #Update the loop exit identifiers
-            if go_back == True:
-                running = False
-                exit_loop = True
-                go_main = True
-
-            if display_text:
-                display()
-                print("testing")
-
-            pygame.display.flip()
-        
-
-    #Break out of the loop to different menus
-    if exit_loop == True:
-        if go_settings == True:
-            display_settings_menu()
-        elif go_main == True:
-            display_main_menu()
-    else:
-        pygame.quit()
-        sys.exit()
-
 
 display_main_menu()
-
 
